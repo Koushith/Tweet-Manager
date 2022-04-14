be27960382034189170043c84b5c31353a244b7d
koushith <koushith97@gmail.com>
2022-04-15 02:49:38 +0530

    adds new link feature

---
+++ src/data/seed.ts
@@ -0,0 +1,24 @@
+import { v4 as uuid } from 'uuid';
+
+export const allLinks = [
+  {
+    id: uuid(),
+    title: 'Material You Design System by Google',
+    description:
+      'The Material You philosophy is set to redefine design not just for Android but also for almost every visual interface designed by Google.',
+    link: 'https://google.in',
+    image: 'image',
+    tags: ['design', 'code'],
+    category: 'Web3',
+  },
+  {
+    id: uuid(),
+    title: 'Material You Design System by Google',
+    description:
+      'The Material You philosophy is set to redefine design not just for Android but also for almost every visual interface designed by Google.',
+    link: 'https://google.in',
+    image: 'image',
+    tags: ['design', 'code'],
+    category: 'React',
+  },
+];
